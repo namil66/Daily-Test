54cb8e5a85008ebad3dd90e6a2a92f9290f0dd92
namilc <angeroi755@gmail.com>
2026-01-29 17:44:16 +0900

    [feat] 다른 사람 풀이 추가

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -3,3 +3,17 @@ function solution(num1, num2) {
     return result
     
 }
+
+//--------------------------------------------
+// 다른 사람의 풀이
+
+/* trunc 메서드 활용 예시
+
+function solution(num1, num2) {
+    return Math.trunc(num1 / num2 * 1000);
+}
+
+Math.trunc()는 소수점 이하는 다 버린다. ex) Math.trunc(23.3) = 23, Math.trunc(-23.3) = -23
+Math.floor()는 소수점을 내림한다. ex) Math.floor(23.3) = 23, 
+Math.floor(-23.3) = -24가 된다. 음수인 경우도 생각해서 소수점을 이하 수를 없애고 싶을때는 Math.trunc를 사용하는 것이 좋다.
+*/
--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -9,3 +9,25 @@ function solution(angle) {
        return 4
 }
 }
+
+// -------------------------------------------------------------------------------------------------------
+// 다른 풀이들
+
+/* 1. filter 메서드 사용
+function solution(angle) {
+   return [0, 90, 91, 180].filter(x => angle>=x).length;
+}
+*/
+
+/* 2. 삼항 연산자 사용
+function solution(angle) {
+    return angle < 90 ? 1 : angle === 90 ? 2 : angle < 180 ? 3 : 4;
+}
+*/
+
+/* 3. 삼항 연산자 및 &&(and) 활용
+function solution(angle) {
+    //예각일 때 1, 직각일 때 2, 둔각일 때 3, 평각일 때 4
+    return angle<90? 1 : (angle>90 && angle<180)? 3 : (angle===180)? 4 : 2;;
+}
+*/
