6c5a235b43cab513ff341f68e7ef987a5eb360f9
namilc <angeroi755@gmail.com>
2026-02-09 00:17:46 +0900

    [feat] 두 수의 차 구하기 - 다른 사람 풀이 추가

--- 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.js
+++ 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.js
@@ -2,3 +2,8 @@ function solution(num1, num2) {
     let result = (num1 - num2)
     return result
 }
+
+// 다른 사람 풀이
+//------------------------------------------------------------------
+// 화살표 함수 사용
+// const solution = (num1, num2) => num1 - num2
